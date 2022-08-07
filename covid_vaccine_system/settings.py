1d9a54329976c3adf729a5f7c1ad0370f72795c8
Ayodele David <dayodele89@outlook.com>
2022-08-07 20:50:17 +0100

    Procfile update

--- covid_vaccine_system/settings.py
+++ covid_vaccine_system/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8aev6dx$&#(j=%dq+nsj%t+!*3m2f&dkjh3%m384o5!tddmj=x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['covid-vaccine-system.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
